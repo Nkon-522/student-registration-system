7367ae7be708d189cd35e37ecaaf6dec74183700
Nkon-522 <nkon.cs@gmail.com>
2024-02-18 22:19:18 -0500

    Add - Add Student layout

--- src/main/java/org/nkon/studentmanagementsystem/Controllers/MainMenuController.java
+++ src/main/java/org/nkon/studentmanagementsystem/Controllers/MainMenuController.java
@@ -3,17 +3,42 @@ package org.nkon.studentmanagementsystem.Controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
+import org.nkon.studentmanagementsystem.Managers.MainAppManager;
 import org.nkon.studentmanagementsystem.Mediators.LoginMainMenuMediator;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
+    private String email;
+
     @FXML
     private Text Greetings;
 
     public void setGreetings(String email) {
+        this.email = email;
         Greetings.setText("Welcome "+email+"!");
+    }
+
+    @FXML
+    private void handleHomeMenuItemClick() throws IOException {
+        LoginMainMenuMediator.getInstance().handleShowMainMenuScene(email);
+    }
+
+    @FXML
+    private void handleLogoutMenuItemClick() throws IOException {
+        LoginMainMenuMediator.getInstance().handleShowLoginScene();
+    }
+
+    @FXML
+    private void handleAboutMenuItemClick() {
+        System.out.println("ABOUT");
+    }
+
+    @FXML
+    private void handleAddStudentButton() throws IOException {
+        MainAppManager.loadNewScene("addStudent.fxml");
     }
 
     @Override
